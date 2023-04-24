23ea52828d09231ad2af48ecba308e0f6fd2b5c1
li1553770945 <1553770945@qq.com>
2023-04-24 17:11:22 +0800

    feat(chat):优化聊天体验，添加状态提示、失败重试、一键复制、回车发送功能

--- src/pages/chat.vue
+++ src/pages/chat.vue
@@ -1,4 +1,9 @@
 <template>
+    <div class="status-bar" :class="'status-bar-' + connectStatus">
+        {{ statusText() }}
+    </div>
+
+
     <div class="chat-container">
         <div class="header" v-if="chatID == ''">
             <el-row>
@@ -12,7 +17,7 @@
                     <el-input v-model="inputChatID" placeholder="请输入聊天ID" />
                 </el-col>
                 <el-col span="4" style="margin:1rem;">
-                    <el-button @click="joinChat">加入聊天</el-button>
+                    <el-button @click="joinChat(inputChatID)">加入聊天</el-button>
                 </el-col>
             </el-row>
         </div>
@@ -29,26 +34,41 @@
                         <template #header>
                             <div class="card-header">
                                 <span>{{ message.sendBySelf == true ? "您" : "对方" }}于{{ message.time }}</span>
-                                <el-button class="button" text>复制</el-button>
+                                <el-button class="button" text @click="copy(message.content)">复制</el-button>
                             </div>
                         </template>
-                        {{ message.content }}
+                        <pre> {{ message.content }}</pre>
                     </el-card>
                 </div>
             </div>
         </el-scrollbar>
 
         <div class="input-box">
-            <el-input v-model="inputMessage" class="input" placeholder="请输入消息内容" type="textarea" :rows="10" />
-            <el-button @click="sendMessage" class="send-btn" :disabled="chatID == '' || sending">发送</el-button>
+            <el-input v-model="inputMessage" class="input" placeholder="请输入消息内容，按下回车键发送" type="textarea" :rows="10"
+                @keydown.enter.prevent="sendMessage" />
+            <el-button @click="sendMessage" class="send-btn"
+                :disabled="connectStatus != 'connected' || sending">发送</el-button>
         </div>
     </div>
+
+    <el-dialog v-model="dialogVisible" title="连接失败" width="30%">
+
+        <span>与服务器连接失败，请刷新页面并重新创建对话</span>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="dialogVisible = false" type="primary">确认</el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
   
 <script lang="ts" setup>
 import { reactive, ref, nextTick } from 'vue';
 import { ElNotification } from 'element-plus';
+import { stringify } from 'querystring';
+import useClipboard from 'vue-clipboard3'
 
+const { toClipboard } = useClipboard()
 interface ClientMessage {
     id: string;
     content: string;
@@ -57,6 +77,7 @@ interface ClientMessage {
     status: string,
     error_msg: string,
 }
+
 const sending = ref(false);
 const chatID = ref('');
 const inputChatID = ref('');
@@ -65,7 +86,11 @@ const inputMessage = ref('');
 const messages: ClientMessage[] = reactive([]);
 const scrollContainerRef: any = ref(null);
 const scrollContentRef: any = ref(null);
+const connectStatus = ref("not-connected"); // not-connected   connected  connect-interupt connect-fail
+const connectRetryNum = ref(0);
+const dialogVisible = ref(false);
 let globalWs: WebSocket;
+
 const handleMessage = (e: MessageEvent) => {
     const jsonString = e.data as string;
     const obj = JSON.parse(jsonString); // 解析 JSON 字符串为 JavaScript 对象
@@ -81,18 +106,54 @@ const handleMessage = (e: MessageEvent) => {
             message: obj.error_msg,
             type: 'error',
         })
+    } else if (obj.type == "warning") {
+        ElNotification({
+            title: '警告',
+            message: obj.error_msg,
+            type: 'warning',
+        })
     }
-
 }
 const createChat = () => {
-    globalWs = new WebSocket('wss://' + window.location.host + "/socket/new-chat")
+    if (window.location.host.match("localhost") != null || window.location.host.match("127.0.0.1") != null) {
+        globalWs = new WebSocket('ws://' + window.location.host + "/socket/new-chat")
+
+    }
+    else {
+        globalWs = new WebSocket('wss://' + window.location.host + "/socket/new-chat")
+
+    }
     initWs(globalWs)
+
 }
-const joinChat = () => {
-    globalWs = new WebSocket('wss://' + window.location.host + "/socket/join-chat?chat_id=" + inputChatID.value)
+const joinChat = (joinChatID: string, joinClientID: string = "") => {
+    console.log("加入聊天");
+    var addr = ""
+    if (window.location.host.match("localhost") != null || window.location.host.match("127.0.0.1") != null) {
+        addr = "ws:"
+
+    }
+    else {
+        addr = "wss:"
+
+    }
+    addr = addr + window.location.host + "/socket/join-chat?chat_id=" + joinChatID;
+    if (joinClientID != "") {
+        addr = addr + "&client_id=" + joinClientID;
+    }
+
+    globalWs = new WebSocket(addr);
     initWs(globalWs)
 }
 const sendMessage = () => {
+    if (connectStatus.value != "connected") {
+        ElNotification({
+            title: '发送失败',
+            message: "未连接至服务器,请检查连接状态",
+            type: 'error',
+        })
+        return;
+    }
     if (inputMessage.value == "") {
         ElNotification({
             title: '发送失败',
@@ -153,8 +214,33 @@ const initWs = (ws: WebSocket) => {
     ws.addEventListener('error', handleError, false)
     ws.addEventListener('message', handleMessage, false)
 }
-
-
+const statusText = () => {
+    switch (connectStatus.value) {
+        case "not-connected":
+            return "未连接至服务器,请创建或加入对话";
+        case "connected":
+            return "已连接至服务器";
+        case "connect-interupt":
+            return "连接中断，正在尝试第" + connectRetryNum.value.toString() + "次重新连接";
+        case "connect-fail":
+            return "与服务器连接失败，请刷新页面并重新创建对话"
+    }
+}
+const copy = async (text: string) => {
+    try {
+        await toClipboard(text)
+        ElNotification({
+            title: '复制成功',
+            type: 'success',
+        })
+    } catch (e) {
+        ElNotification({
+            title: '复制失败',
+            type: 'error',
+        })
+        console.log("复制失败：", e);
+    }
+}
 
 function handleOpen(e: Event) {
     ElNotification({
@@ -162,21 +248,41 @@ function handleOpen(e: Event) {
         type: 'success',
         message: "websocket连接成功"
     })
+    connectStatus.value = "connected";
+
 }
 function handleClose(e: CloseEvent) {
+    console.log("连接已关闭")
     ElNotification({
         title: '连接失败',
         message: "websocket连接中断:" + e.code,
         type: 'error',
     })
-    chatID.value = "";
-    clientID.value = "";
+    if (connectRetryNum.value == 4 || chatID.value == "") {
+        dialogVisible.value = true;
+        nextTick(() => {
+            connectStatus.value = "connect-fail";
+        })
+        return;
+    } else {
+        nextTick(() => {
+            connectStatus.value = "connect-interupt";
+        })
+        connectRetryNum.value = connectRetryNum.value + 1;
+        joinChat(chatID.value, clientID.value);
+    }
+
 }
 function handleError(e: Event) {
+
     ElNotification({
         title: '请求失败',
         message: "websocket连接失败",
         type: 'error',
+    })
+    nextTick(() => {
+        console.log("连接失败")
+        connectStatus.value = "connect-fail";
     })
 }
 </script>
@@ -244,6 +350,28 @@ function handleError(e: Event) {
     border: none;
     border-radius: 4px;
     cursor: pointer;
+}
+
+.status-bar {
+    /* 设置顶部文字的样式 */
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    font-size: 16px;
+}
+
+.status-bar-connected {
+    /* 设置连接成功状态的背景色和文字颜色 */
+    background-color: green;
+    color: white;
+}
+
+.status-bar-not-connected,
+.status-bar-connect-fail,
+.status-bar-connect-interupt {
+    /* 设置连接失败状态的背景色和文字颜色 */
+    background-color: red;
+    color: white;
 }
 </style>
   
--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
         },
         '/socket': { // 匹配请求路径，
           ws:true,
-          target: 'wss://localhost:8000/', // 代理的目标地址
+          target: 'ws://localhost:8000/', // 代理的目标地址
           changeOrigin: true,
       }
     }
