8cdbcb577c3f871031127f1059f887536f74b817
li1553770945 <1553770945@qq.com>
2024-04-11 00:41:44 +0800

    feat(visual):添加js大小分析工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig,type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { resolve } from "path"; 
+import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: "test.html", //分析图生成的文件名
+      open: true //如果存在本地服务端口，将在打包后自动展示
+    }) as PluginOption,
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
@@ -16,28 +23,29 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
+  base: "./", // 设置打包路径
   resolve: {
     // ↓路径别名
     alias: {
       "@": resolve(__dirname, "./src"),
-      "@pages":resolve(__dirname,"./src/pages"),
-      "@components":resolve(__dirname,"./src/components"),
-      "@style":resolve(__dirname,"./src/style"),
+      "@pages": resolve(__dirname, "./src/pages"),
+      "@components": resolve(__dirname, "./src/components"),
+      "@style": resolve(__dirname, "./src/style"),
     }
   },
-  
+
   server: {
     proxy: {
-        '/api': { // 匹配请求路径，
-            target: 'http://localhost:8000/', // 代理的目标地址
-            changeOrigin: true,
-        },
-        '/socket': { // 匹配请求路径，
-          ws:true,
-          target: 'ws://localhost:8000/', // 代理的目标地址
-          changeOrigin: true,
+      '/api': { // 匹配请求路径，
+        target: 'http://localhost:8000/', // 代理的目标地址
+        changeOrigin: true,
+      },
+      '/socket': { // 匹配请求路径，
+        ws: true,
+        target: 'ws://localhost:8000/', // 代理的目标地址
+        changeOrigin: true,
       }
     }
-}
+  }
 
 })
