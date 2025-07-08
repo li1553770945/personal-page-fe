abf9fd0e3b3af0d7e7c6bce7675663e9fa2ce54c
li1553770945 <1553770945@qq.com>
2025-07-08 23:26:19 +0800

    feat:修改visualize文件名称

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
       gzipSize: true,
       brotliSize: true,
       emitFile: false,
-      filename: "test.html", //分析图生成的文件名
+      filename: "rollup-visualize.html", //分析图生成的文件名
       open: true //如果存在本地服务端口，将在打包后自动展示
-    }) as PluginOption,
+    }) as unknown as PluginOption,
     viteCompression({
       algorithm	: 'brotliCompress',
       threshold: 1024,
